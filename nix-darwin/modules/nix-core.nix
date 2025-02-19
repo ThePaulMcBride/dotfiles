ff2dd0adc2590a7a9d5ea0c91dbb94438ea0c39a
Paul McBride <hello@paulmcbride.com>
2025-02-18 19:09:34 -0800

    chore: update deps

--- nix-darwin/modules/nix-core.nix
+++ nix-darwin/modules/nix-core.nix
@@ -7,17 +7,16 @@
     "flakes"
   ];
 
+  # required for nix to work with Determinate
+  nix.enable = false;
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
 
   nix.package = pkgs.nix;
 
   # do garbage collection weekly to keep disk usage low
   nix.gc = {
-    automatic = lib.mkDefault true;
     options = lib.mkDefault "--delete-older-than 7d";
   };
 
