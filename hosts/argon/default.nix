0fb4e9fe41038ea9f94961ec2012b8c44c0dc477
Paul McBride <hello@paulmcbride.com>
2025-06-12 23:58:41 -0700

    chore: move nixos config to repo

---
+++ hosts/argon/default.nix
@@ -0,0 +1,187 @@
+# Edit this configuration file to define what should be installed on 
+# your system.  Help is available in the configuration.nix(5) man page 
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+{ imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true; 
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 ''; 
+
+  hardware = {
+    enableAllFirmware = true;
+    graphics = {
+      enable = true;
+    };
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          Name = "Hello";
+	  ControllerMode = "dual";
+          FastConnectable = "true";
+          Experimental = "true";
+        };
+        Policy = {
+          AutoEnable = "true";
+        };
+      };
+    };
+    xpadneo.enable = true;
+    steam-hardware.enable = true; 
+    uinput.enable = true;
+  };
+
+  networking.hostName = "argon"; # Define your hostname.
+  # networking.wireless.enable = true; # Enables wireless support via 
+  # wpa_supplicant.
+
+  # Configure network proxy if necessary networking.proxy.default = 
+  # "http://user:password@proxy:port/"; networking.proxy.noProxy = 
+  # "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = { LC_ADDRESS = "en_US.UTF-8"; 
+    LC_IDENTIFICATION = "en_US.UTF-8"; LC_MEASUREMENT = "en_US.UTF-8"; 
+    LC_MONETARY = "en_US.UTF-8"; LC_NAME = "en_US.UTF-8"; LC_NUMERIC = 
+    "en_US.UTF-8"; LC_PAPER = "en_US.UTF-8"; LC_TELEPHONE = 
+    "en_US.UTF-8"; LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable the X11 windowing system
+  services.xserver = {
+    enable = true;
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+    desktopManager.gnome = {
+      enable = true;
+      extraGSettingsOverridePackages = [ pkgs.mutter ];
+      extraGSettingsOverrides = ''
+        [org.gnome.mutter]
+        experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
+      '';
+    };
+    xkb = { layout = "us"; variant = ""; };
+
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false; security.rtkit.enable = true; 
+  services.pipewire = {
+    enable = true; alsa.enable = true; alsa.support32Bit = true; 
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this jack.enable = 
+    #true;
+
+    # use the example session manager (no others are packaged yet so 
+    # this is enabled by default, no need to redefine it in your config 
+    # for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager). 
+  # services.xserver.libinput.enable = true;
+
+  services.udev = {
+    packages = with pkgs; [
+      game-devices-udev-rules
+    ];
+  };
+  
+  
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.paul = { isNormalUser = true; description = "Paul McBride"; extraGroups = [ "networkmanager" "wheel" ]; packages = 
+    with pkgs; [
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    gamescopeSession.enable = true;
+  }; 
+
+  programs.gamemode.enable = true;
+
+  programs.nix-ld.enable = true;
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "paul" ];
+  };
+
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run: $ nix 
+  # search wget
+  environment.systemPackages = with pkgs; [
+    appimage-run
+    azahar
+    brave
+    cemu
+    gearlever
+    ghostty
+    git
+    lact
+    lutris
+    mangohud
+    melonDS
+    mgba
+    neovim
+    ryujinx
+    steam-rom-manager
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are 
+  # started in user sessions. programs.mtr.enable = true; 
+  # programs.gnupg.agent = {
+  #   enable = true; enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon. services.openssh.enable = true;
+
+  # Open ports in the firewall. networking.firewall.allowedTCPPorts = [ 
+  # ... ]; networking.firewall.allowedUDPPorts = [ ... ]; Or disable the 
+  # firewall altogether. networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default 
+  # settings for stateful data, like file locations and database 
+  # versions on your system were taken. It‘s perfectly fine and 
+  # recommended to leave this value at the release version of the first 
+  # install of this system. Before changing this value read the 
+  # documentation for this option (e.g. man configuration.nix or on 
+  # https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
+
