5efce2f9f6b0bc3c8454144d93ba50b181b339b0
Paul McBride <hello@paulmcbride.com>
2026-02-25 15:38:07 -0800

    Show user list instead of username field on login screen

--- modules/nix-darwin.nix
+++ modules/nix-darwin.nix
@@ -147,7 +147,7 @@
 
       loginwindow = {
         GuestEnabled = false; # disable guest user
-        SHOWFULLNAME = true; # show full name in login window
+        SHOWFULLNAME = false; # show user list in login window
       };
     };
 
