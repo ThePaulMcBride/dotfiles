d78d46b2dfbfae87cc25719d3fe908cec8a38922
Paul McBride <hello@paulmcbride.com>
2026-02-25 19:23:40 -0800

    Configure ruby formatters

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,36 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false },
-    servers = {
-      ruby_lsp = {
-        mason = false,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        ruby_lsp = {
+          mason = false,
+        },
+        rubocop = {
+          enabled = false,
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ruby = { "standardrb", "rubocop", stop_after_first = true },
+      },
+      formatters = {
+        standardrb = {
+          command = "bundle",
+          prepend_args = { "exec", "standardrb" },
+          condition = function(_, ctx)
+            return vim.fs.find(".standard.yml", { path = ctx.dirname, upward = true })[1] ~= nil
+          end,
+        },
+        rubocop = {
+          command = "bundle",
+          prepend_args = { "exec", "rubocop" },
+        },
       },
     },
   },
