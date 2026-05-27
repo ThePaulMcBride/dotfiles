9eac742575c70b66c4f76684baa87943b224cac2
Paul McBride <hello@paulmcbride.com>
2026-05-26 21:01:26 -0700

    Update eden

--- pkgs/eden-emulator.nix
+++ pkgs/eden-emulator.nix
@@ -1,15 +1,11 @@
-{ stdenvNoCC, fetchurl, dpkg, }:
+{ stdenvNoCC, fetchurl }:
 let
   pname = "eden-emulator";
-  version = "0.2.0-rc2";
+  version = "0.2.0";
 
   appImageUrl =
     "https://stable.eden-emu.dev/v${version}/Eden-Linux-v${version}-amd64-clang-pgo.AppImage";
-  appImageHash = "sha256-/4qSE97tO9klkmliAJzR4UnnYPrdSqmzrwItFoQaiAU=";
-
-  debUrl =
-    "https://stable.eden-emu.dev/v${version}/Eden-Debian-13-v${version}-amd64.deb";
-  debHash = "sha256-9qqe1qaRNhaYwlMqaE3KaQXUH7/TJJoxtEHB8fVeEwg=";
+  appImageHash = "sha256-aMi1rOl3KwAWpzx3CJlouEcI2s4GrlyaRy4h+rAwRl8=";
 in stdenvNoCC.mkDerivation {
   inherit pname version;
 
@@ -18,39 +14,13 @@ in stdenvNoCC.mkDerivation {
     hash = appImageHash;
   };
 
-  debSrc = fetchurl {
-    url = debUrl;
-    hash = debHash;
-  };
-
   dontUnpack = true;
-
-  nativeBuildInputs = [ dpkg ];
 
   installPhase = ''
     runHook preInstall
 
     install -Dm755 "$src" "$out/bin/eden-emulator"
     ln -s eden-emulator "$out/bin/eden"
-
-    debRoot="$TMPDIR/eden-deb"
-    mkdir -p "$debRoot"
-    dpkg-deb -x "$debSrc" "$debRoot"
-
-    install -Dm644 \
-      "$debRoot/usr/share/icons/hicolor/scalable/apps/dev.eden_emu.eden.svg" \
-      "$out/share/icons/hicolor/scalable/apps/dev.eden_emu.eden.svg"
-
-    install -Dm644 \
-      "$debRoot/usr/share/applications/dev.eden_emu.eden.desktop" \
-      "$out/share/applications/dev.eden_emu.eden.desktop"
-
-    substituteInPlace "$out/share/applications/dev.eden_emu.eden.desktop" \
-      --replace-fail "Exec=eden" "Exec=eden-emulator" \
-      --replace-fail "TryExec=eden" "TryExec=eden-emulator"
-
-    substituteInPlace "$out/share/applications/dev.eden_emu.eden.desktop" \
-      --replace "TryExec=eden-emulator-emulator" "TryExec=eden-emulator"
 
     runHook postInstall
   '';
